65dcf2b93e4acf2be2c19fb51331ec08e426f6f9
Diego Bizerra da Silva <diegorj93@gmail.com>
2024-01-30 17:14:03 -0300

    Configuração do projeto

---
+++ src/main/java/br/com/cotiinformatica/api/cliente/ClientesapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.api.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesapiApplication.class, args);
+	}
+
+}
