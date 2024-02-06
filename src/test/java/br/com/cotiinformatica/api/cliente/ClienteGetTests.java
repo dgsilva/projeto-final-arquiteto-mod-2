7eb906bc97770a36179e3aea0676b767974dce9b
Diego Bizerra da Silva <diegorj93@gmail.com>
2024-02-06 17:49:08 -0300

    Desenvolvimento dos testes

---
+++ src/test/java/br/com/cotiinformatica/api/cliente/ClienteGetTests.java
@@ -0,0 +1,40 @@
+package br.com.cotiinformatica.api.cliente;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ClienteGetTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	@Test
+    public void clientesGetTest() throws Exception {
+        mockMvc.perform(get("/api/clientes")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                
+    }
+	
+	
+
+
+}
